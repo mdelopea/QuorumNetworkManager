8a294d73fa879f1a6da7853c745372a4e1b75d45
mdelopea <48993826+mdelopea@users.noreply.github.com>
2019-03-28 09:48:29 +0100

    Update startRaftNode.sh

--- startRaftNode.sh
+++ startRaftNode.sh
@@ -8,19 +8,21 @@ nohup constellation-node constellation.config &> constellation.log &
 
 sleep 5
 
-FLAGS="--datadir Blockchain --targetgaslimit $3 --shh --port $4 --unlock 0 --password passwords.txt --raft"
-RPC_API="admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum,raft"
-HTTP_RPC_ARGS="--rpc --rpcaddr 0.0.0.0 --rpcport $5 --rpcapi $RPC_API --rpccorsdomain '*' --ethstats $1"
-WS_RPC_ARGS="--ws --wsaddr 0.0.0.0 --wsport $6 --wsapi $RPC_API --wsorigins=*"
-RAFT_ARGS="--raftport $7"
+FLAGS="--datadir Blockchain --targetgaslimit $1 --shh --port $2 --unlock 0 --password passwords.txt --raft"
 
-if [ "$8" == "permissionedNodes" ]
+RPC_API="admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum,raft"
+HTTP_RPC_ARGS="--rpc --rpcaddr 0.0.0.0 --rpcport $3 --rpcapi $RPC_API"
+WS_RPC_ARGS="--ws --wsaddr 0.0.0.0 --wsport $4 --wsapi $RPC_API --wsorigins=*"
+
+RAFT_ARGS="--raftport $5"
+
+if [ "$6" == "permissionedNodes" ]
   then
   RAFT_ARGS="$RAFT_ARGS --permissioned Blockchain"
 fi
-if [ "$#" == 9 ]
+if [ "$#" == 7 ]
   then
-  RAFT_ARGS="$RAFT_ARGS --raftjoinexisting $9"
+  RAFT_ARGS="$RAFT_ARGS --raftjoinexisting $7"
 fi
 
 ALL_ARGS="$FLAGS $HTTP_RPC_ARGS $WS_RPC_ARGS $RAFT_ARGS"
